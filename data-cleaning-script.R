23f03c6e5a1c8339fde04915cb845fd6a1d94869
Lily Russell <lilyrussell2017@gmail.com>
2026-08-21 10:09:03 +0930

    Organising and cleaning up data cleaning file

--- data-cleaning-script.R
+++ data-cleaning-script.R
@@ -1,27 +1,35 @@
 #| label: read-clean-combine-write
 #| eval: false
 
-# install.packages(c("tidyverse", "here", "lubridate", "glue"))
+
+# Installing packages for cleaning ----------------------------------------
 library(tidyverse)   # dplyr, ggplot2, tidyr, readr, stringr, forcats
 library(here)        # project-relative paths; don't use setwd()
 library(lubridate)   # dates
 library(glue)        # easy string manipulation
 
-# set up directories
+
+# Set up directories ------------------------------------------------------
+
 raw_dir   <- here("data", "raw")
 clean_dir <- here("data", "clean")
 
-# Election day = first Tuesday after the first Monday in November.
-election_day <- c("2012" = as.Date("2012-11-06"),
-                  "2016" = as.Date("2016-11-08"))
 
-# read in raw data
+# Reading in raw data -----------------------------------------------------
+
 polls_2016_raw <- read_csv(glue("{raw_dir}/state_polls_2016.csv"))
 polls_2012_raw <- read_csv(glue("{raw_dir}/state_polls_2012.csv"))
 results_1976_2024_raw <- read_csv(glue("{raw_dir}/1976-2024-president.csv"))
 
-# clean datasets
+
+# Cleaning the datasets ---------------------------------------------------
+
 # Time to election
+# Define election day = first Tuesday after the first Monday in November.
+election_day <- c("2012" = as.Date("2012-11-06"),
+                  "2016" = as.Date("2016-11-08"))
+
+# Adding time to election as column to data sets
 polls_2016 <- polls_2016_raw |>
   mutate(
     start_date = as.Date(start_date),
@@ -34,18 +42,8 @@ polls_2012 <- polls_2012_raw |>
     end_date   = as.Date(end_date),
     days_to_election = as.integer(election_day["2012"] - end_date)
   )
-polls_2016 |>
-  ggplot(aes(days_to_election)) +
-  geom_histogram(binwidth = 7) +
-  scale_x_reverse() +
-  labs(x = "Days to election (binned by week)", y = "Number of polls")
-polls_2012 |>
-  ggplot(aes(days_to_election)) +
-  geom_histogram(binwidth = 7) +
-  scale_x_reverse() +
-  labs(x = "Days to election (binned by week)", y = "Number of polls")
 
-# extracting the year and candidate name for each row
+# Extracting the year and candidate name for each row
 polls_2016 <- polls_2016_raw |>
   mutate(
     year = str_extract(poll_info, "^\\d{4}"),
@@ -57,13 +55,14 @@ polls_2012 <- polls_2012_raw |>
     state = str_extract(poll_info, "(?<=^\\d{4}-).*?(?=-president)")
   )
 
-# creating longer data sets with each candidate on a new row
-candidate_cols_2016 <- c("Trump", "Clinton", "Johnson",
-                         "McMullin", "Other", "Undecided")
+# Creating longer data sets with each candidate on a new row
+candidate_cols_2016 <- c("Trump", "Clinton", "Johnson", "McMullin", 
+                         "Other", "Undecided")
 polls_2016_long <- polls_2016 |>
   pivot_longer(cols = all_of(candidate_cols_2016),
                values_transform = as.numeric,
                names_to = "candidate", values_to = "pct")
+
 candidate_cols_2012 <- c("Obama", "Romney", "Other", "Undecided")
 polls_2012_long <- polls_2012 |>
   pivot_longer(cols = all_of(candidate_cols_2012),
@@ -71,7 +70,9 @@ polls_2012_long <- polls_2012 |>
                names_to = "candidate", values_to = "pct")
 
 
-# combine datasets as required
+# Combining datasets into long set ----------------------------------------
+# Removing negative population values
+
 polls_long <- bind_rows(polls_2012_long, polls_2016_long) |>
   mutate(sample_size = na_if(sample_size, -1),
          across(c(sample_subpopulation, mode, partisanship,
@@ -79,9 +80,11 @@ polls_long <- bind_rows(polls_2012_long, polls_2016_long) |>
                 factor)
   )
 
-# save datasets (ASK MONDAY)
-write_csv(polls_long, "data/clean/polls_long_clean.csv")
-write_csv(polls_2012, "data/clean/polls_2012_clean.csv")
-write_csv(polls_2016, "data/clean/polls_2016_clean.csv")
-write_csv(polls_2012_long, "data/clean/polls_2012_long_clean.csv")
-write_csv(polls_2016_long, "data/clean/polls_2016_long_clean.csv")
+
+# Saving data sets as cvs in clean folder ---------------------------------
+
+write_csv(polls_long, "data/clean/polls_long_clean.csv")    # combined date
+write_csv(polls_2012, "data/clean/polls_2012_clean.csv")    # clean 2012 data
+write_csv(polls_2016, "data/clean/polls_2016_clean.csv")    # clean 2016 data
+write_csv(polls_2012_long, "data/clean/polls_2012_long_clean.csv")  # 2012 long
+write_csv(polls_2016_long, "data/clean/polls_2016_long_clean.csv")  # 2016 long
